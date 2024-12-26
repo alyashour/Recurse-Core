1dd15ba0b60059496641d1f425efa6be06c68466
Aly Ashour <alyashour1@gmail.com>
2024-12-26 11:02:46 -0500

    added task manager

---
+++ task/task_manager.go
@@ -0,0 +1,35 @@
+package task
+
+import (
+	"fmt"
+	"time"
+)
+
+type TaskManager struct {
+	fileManager FileManager
+	Path        string
+	Tasks       []Task
+}
+
+type FileManager interface {
+}
+
+func (tm TaskManager) CreateTask(title, description, assignee string, dueDate time.Time) (*Task, error) {
+	return nil, fmt.Errorf("not implemented error")
+}
+
+func (tm TaskManager) GetTask(id int) (*Task, error) {
+	return nil, fmt.Errorf("not implemented error")
+}
+
+func (tm TaskManager) UpdateTask(id int, updates Task) error {
+	return fmt.Errorf("not implemented error")
+}
+
+func (tm TaskManager) DeleteTask(id int) error {
+	return fmt.Errorf("not implemented error")
+}
+
+func (tm TaskManager) ListTasks() ([]Task, error) {
+	return nil, fmt.Errorf("not implemented error")
+}
